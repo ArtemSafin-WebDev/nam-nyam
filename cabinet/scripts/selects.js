78e9e695d23612b363c19a72f665963a79ab5f24
Artem Safin <artemsafin.webdev@gmail.com>
2025-09-16 00:42:12 +0300

    по задачам

---
+++ cabinet/scripts/selects.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const selects = Array.from(document.querySelectorAll(".js-select"));
+  selects.forEach((select) => {
+    const button = select.querySelector("button");
+    const buttonText = button.querySelector("span:first-child");
+    const placeholder = button.getAttribute("data-placeholder");
+    const options = Array.from(select.querySelectorAll("input"));
+
+    const setActive = () => {
+      const checkedInput = options.find((option) => option.checked);
+      if (!checkedInput) {
+        buttonText.textContent = placeholder;
+        button.classList.add("placeholder-shown");
+        select.classList.remove("active");
+        return;
+      }
+      buttonText.textContent =
+        checkedInput.parentElement.querySelector("span:last-child").textContent;
+
+      button.classList.remove("placeholder-shown");
+      select.classList.remove("active");
+    };
+
+    setActive();
+
+    options.forEach((option) => option.addEventListener("change", setActive));
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      select.classList.toggle("active");
+    });
+  });
+});
