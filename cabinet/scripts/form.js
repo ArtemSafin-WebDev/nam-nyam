37ec025a4ef7095d0b79a8ae6e524f21e14732d1
Artem Safin <artemsafin.webdev@gmail.com>
2025-09-29 03:22:27 +0300

    По правкам

--- cabinet/scripts/form.js
+++ cabinet/scripts/form.js
@@ -11,28 +11,28 @@ document.addEventListener("DOMContentLoaded", () => {
           body: new FormData(form),
         })
           .then((response) => {
-            // Promise.resolve();
-            // if (!response.ok) {
-            //   throw new Error("Something went wrong");
-            // }
-            // return response.json();
+            Promise.resolve();
+            if (!response.ok) {
+              throw new Error("Something went wrong");
+            }
+            return response.json();
           })
           .then((data) => {
             console.log(data);
-            // Array.from(document.querySelectorAll(".js-modal")).forEach(
-            //   (modal) => modal.classList.remove("active")
-            // );
-            // const successModal = document.querySelector("#success-modal");
-            // successModal.classList.add("active");
-            // document.body.classList.add("modal-open");
+            Array.from(document.querySelectorAll(".js-modal")).forEach(
+              (modal) => modal.classList.remove("active")
+            );
+            const successModal = document.querySelector("#success-modal");
+            successModal.classList.add("active");
+            document.body.classList.add("modal-open");
           })
           .catch((error) => {
-            // const errorModal = document.querySelector("#error-modal");
-            // errorModal.classList.add("active");
-            // Array.from(document.querySelectorAll(".js-modal")).forEach(
-            //   (modal) => modal.classList.remove("active")
-            // );
-            // document.body.classList.add("modal-open");
+            Array.from(document.querySelectorAll(".js-modal")).forEach(
+              (modal) => modal.classList.remove("active")
+            );
+            const errorModal = document.querySelector("#error-modal");
+            errorModal.classList.add("active");
+            document.body.classList.add("modal-open");
             console.log(error);
           });
       }
